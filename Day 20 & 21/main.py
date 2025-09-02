35c1ca59079eee4c0132b11678b270cb40d84678
rxchitrx <rachitmital@gmail.com>
2025-09-02 15:28:26 +0300

    Completed Part 3 of the project. Added a new snake.py file. Snake.py file now contains the Snake Class which handles creating and moving the snake. The starting positions and move distance have been changed to constants.

--- Day 20 & 21/main.py
+++ Day 20 & 21/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,31 +8,15 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-blocks = []
-
-for position in starting_positions:
-    new_block = Turtle("square")
-    new_block.color("white")
-    new_block.penup()
-    new_block.goto(position)
-    blocks.append(new_block)
-
-
+snake = Snake()
 
 
 game_is_on = True
-
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for block_num in range (len(blocks) - 1, 0, -1):
-        new_x = blocks[block_num - 1].xcor()
-        new_y = blocks[block_num - 1].ycor()
-        blocks[block_num].goto(new_x, new_y)
+    snake.move()
 
-    blocks[0].forward(20)
-    blocks[0].left(90)
 
 screen.exitonclick()
